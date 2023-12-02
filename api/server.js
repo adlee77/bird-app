cdc225124636d56c7bcf4ab1d9c650e233882b62
Andrew Lee <andrewimg@gmail.com>
2023-12-02 14:46:22 -0800

    updated package .json to utilize proxy server and setup for heroku build

--- api/server.js
+++ api/server.js
@@ -20,7 +20,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 
-app.listen(8800, () => {
+app.listen(8080, () => {
   console.log("Connected!");
 });
 
