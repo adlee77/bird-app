42b15ae38b8a84b864039b011cf13252c9912938
Andrew Lee <andrewimg@gmail.com>
2023-12-02 19:10:02 -0800

    added PORT to dotenv

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-app.listen(8080, () => {
+app.listen(process.env.PORT || 8080, () => {
   console.log("Connected!");
 });
 
